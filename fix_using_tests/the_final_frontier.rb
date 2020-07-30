450e01d849a72fc781fed6931ff502a653b13816
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-30 23:16:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,11 +4,11 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  p "#{state_log}, Greetings, crewmembers!"
+  date = generate_star_date
+  "#{date}, Greetings, crewmembers!"
 end
